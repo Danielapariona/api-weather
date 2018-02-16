c74994bf4983931582111a8c54ff591b86a0b2b0
EstrellaBF <stern.von.nord>
2018-02-16 09:18:17 -0500

    subiendo el clima empezando desde el día sigueinte en el que se esta

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const showMap = document.getElementById('map');
   let latitude;
   let longitude;
@@ -7,13 +7,11 @@ $(document).ready(function() {
   // Función para acceder al api DarkSky
   let getApiWheater = (data) => {
     let currentlyWheater = data.currently;
-    console.log(data);
     // console.log(currentlyWheater.temperature);
     let temperatureFarenheit = currentlyWheater.temperature;
     let temperatureCentigrados = parseInt(((temperatureFarenheit - 32) * 5 / (9.340)), 10);
     // console.log(temperatureCentigrados); // 23° convertidos de farenheit
     let summary = currentlyWheater.summary;
-    console.log(summary);
     let humidity = currentlyWheater.humidity;
     let summary2 = data.daily.summary;
 
@@ -21,6 +19,29 @@ $(document).ready(function() {
     $('#summary').text(summary);
     $('#humidity').text(humidity);
     $('#summary2').text(summary2);
+
+    // Week vista
+    // console.log(data);
+    console.log(data.daily.data);
+    // console.log(data.daily.data[0].time);
+    var d = new Date();
+    var n = d.getDay(data.daily.data[0].time);
+    // console.log(n);
+
+    let $daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    $.each($daysOfWeek, function(index, val) {
+      console.log(index);
+        $('.current-wheater').append(`<div id="day-position-${index}"><p>${data.daily.data[index].summary}</p></div>`);
+        console.log(data.daily.data[index]); // desde la primera posición
+      if(n === index){
+        console.log(index);
+        // console.log(d.getDay(data.daily.data[index].time));
+        console.log(val); // la fecha en este caso Saturday
+        // $('#day-position-')
+      } else {
+        console.log('nada');
+      }
+    });
   };
 
   // En caso el usuario no acepte conocer ubicación
@@ -31,8 +52,7 @@ $(document).ready(function() {
   if (navigator.geolocation) {
     // alert('Puedes usar geolocalización en tu dispositivo');
     let getPosition = (position) => {
-      console.log(position.coords.latitude);
-      console.log(position.coords.longitude);
+      ;
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
       // guardando en localstorage
@@ -51,7 +71,8 @@ $(document).ready(function() {
     alert('No se pudo ubicarte');
   }
 
-  $('#predictions').on('click', function() {
+  $('#predictions').on('click', function () {
     window.location.href = 'views/week.html';
   });
+
 });
